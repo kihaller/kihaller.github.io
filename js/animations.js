6364b2cee5458fbc43244abbd5a775a0289ab785
kihaller <67028582+kihaller@users.noreply.github.com>
2020-08-24 12:35:04 -0700

    Added animations navbar and featurettes

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,10 @@
+// Fade in titles
 (function () {
   var elements;
   var windowHeight;
 
   function init() {
-    elements = document.querySelectorAll(".hidden");
+    elements = document.querySelectorAll(".fade");
     windowHeight = window.innerHeight;
   }
 
@@ -24,3 +25,66 @@
   init();
   checkPosition();
 })();
+
+// Slide in featurettes
+
+//slide in from left
+(function () {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".slider-left");
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (element of elements) {
+      var positionFromTop = element.getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight / 2 <= 0) {
+        element.classList.add("slide-in-from-left");
+        element.classList.remove("hidden");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+})();
+
+//slide in from right
+(function () {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".slider-right");
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (element of elements) {
+      var positionFromTop = element.getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight / 2 <= 0) {
+        element.classList.add("slide-in-from-right");
+        element.classList.remove("hidden");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+})();
+
+//Navbar scroll
+$(window).scroll(function () {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 56);
+});
