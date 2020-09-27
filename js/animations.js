918332d889d97461a694e82072b89886fe0352a4
kihaller <67028582+kihaller@users.noreply.github.com>
2020-09-27 16:02:37 -0700

    Added project

--- js/animations.js
+++ js/animations.js
@@ -28,51 +28,27 @@
 
 // Slide in featurettes
 
-//slide in from left
+//slide in from left or right
 (function () {
   var elements;
   var windowHeight;
 
   function init() {
-    elements = document.querySelectorAll(".slider-left");
+    elements = document.querySelectorAll(".slider");
     windowHeight = window.innerHeight;
   }
 
   function checkPosition() {
-    for (element of elements) {
-      var positionFromTop = element.getBoundingClientRect().top;
+    for (let i = 0; i < elements.length; i++) {
+      var positionFromTop = elements[i].getBoundingClientRect().top;
 
       if (positionFromTop - (3 * windowHeight) / 4 <= 0) {
-        element.classList.add("slide-in-from-left");
-        element.classList.remove("hidden");
-      }
-    }
-  }
-
-  window.addEventListener("scroll", checkPosition);
-  window.addEventListener("resize", init);
-
-  init();
-  checkPosition();
-})();
-
-//slide in from right
-(function () {
-  var elements;
-  var windowHeight;
-
-  function init() {
-    elements = document.querySelectorAll(".slider-right");
-    windowHeight = window.innerHeight;
-  }
-
-  function checkPosition() {
-    for (element of elements) {
-      var positionFromTop = element.getBoundingClientRect().top;
-
-      if (positionFromTop - (3 * windowHeight) / 4 <= 0) {
-        element.classList.add("slide-in-from-right");
-        element.classList.remove("hidden");
+        if (i % 2 === 0) {
+          elements[i].classList.add("slide-in-from-left");
+        } else {
+          elements[i].classList.add("slide-in-from-right");
+        }
+        elements[i].classList.remove("hidden");
       }
     }
   }
